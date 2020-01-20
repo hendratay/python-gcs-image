b8c4b4d5ecc186aa7c22460d6b43f40bb08bc7db
hendra.tay <hendra.tay@webimp.com.sg>
2020-01-20 09:52:14 +0700

    add delete serving url route

--- main.py
+++ main.py
@@ -23,9 +23,10 @@ def image_url():
 		return json_response(error, 422)
 
 	filepath = (bucket + "/" + image)
+	blob_key = blobstore.create_gs_key('/gs/' + filepath)
 
 	try:
-		servingImage = images.get_serving_url(None, filename='/gs/' + filepath)
+		servingImage = images.get_serving_url(blob_key)
 	except images.AccessDeniedError:
 		error = json.dumps({'error': 'Ensure the GAE service account has access to the object in Google Cloud Storage.'})
 		return json_response(error, 401)
@@ -43,7 +44,26 @@ def image_url():
 		error = json.dumps({'error': 'There was a problem transforming the image. Ensure the GAE service account has access to the object in Google Cloud Storage.'})
 		return json_response(error, 400)
 
-	return json_response(json.dumps({'image_url': servingImage}))
+	return json_response(json.dumps({'image_url': servingImage, 'blob_key': blob_key}))
+
+@app.route('/delete-image-url', methods=['GET'])
+def delete_image_url():
+	blob_key = request.args.get('blob_key')
+
+	try:
+		images.delete_serving_url(blob_key)
+	except images.BlobKeyRequiredError:
+		error = json.dumps({'error': 'Blob key not found.'})
+		return json_response(error, 400)
+	except images.InvalidBlobKeyError:
+		error = json.dumps({'error': 'Blob key invalid.'})
+		return json_response(error, 400)
+	except images.Error:
+		error = json.dumps({'error': 'Some error occured when deleting serving url.'})
+		return json_response(error, 400)
+
+	return json_response(json.dumps({'message': 'success delete serving url!'}))
+
 
 def json_response(data='', status=200, headers=None):
 	headers = headers or {}
